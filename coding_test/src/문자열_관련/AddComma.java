7e991ca2b9373232f420a930f04afa8f32592c66
IWBTM <rnlduadl2000@naver.com>
2023-03-29 19:40:14 +0900

    가격에 3자리마다 ,를 붙여라.

--- coding_test/src/문자열_관련/AddComma.java
+++ coding_test/src/문자열_관련/AddComma.java
@@ -1,10 +1,31 @@
 package 문자열_관련;
 
+/*
+ * 금액을 입력하면 콤마(,)를 붙여서 리턴하라.
+ * 
+ * ex)
+ * 1000000 -> 1,000,000
+ */
 public class AddComma {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static String priceToString;
+	private static StringBuffer sb;
 
+	public static String addComma(int price) {
+		priceToString = String.valueOf(price);
+		sb = new StringBuffer();
+		sb.append(priceToString);
+
+		for (int i = 3; i < priceToString.length(); i += 3) {
+			sb.insert((priceToString.length() - i), ",");
+		}
+
+		return sb.toString();
 	}
 
+	public static void main(String[] args) {
+		System.out.println(AddComma.addComma(1000000));
+		System.out.println(AddComma.addComma(48613866));
+		System.out.println(AddComma.addComma(77778564));
+	}
 }
